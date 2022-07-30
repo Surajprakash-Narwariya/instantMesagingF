3e5c536dacac86a079fad91a5bef30e33786d0f8
Surajprakash-Narwariya <surajprakash.narwariya@gmail.com>
2022-07-30 14:22:02 +0530

    ch

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-// export const url = 'https://api-quickchat.herokuapp.com';
-export const url = 'http://localhost:4000';
+export const url = 'https://api-quickchat.herokuapp.com';
+// export const url = 'http://localhost:4000';
